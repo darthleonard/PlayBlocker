4d356cd6d30cbf2e2a746fb9c879a647ef4ff83b
darthleonard <leolinuxmx@gmail.com>
2020-03-31 19:41:33 -0600

    password message handled

--- app/src/main/java/darthleonard/archaos/playblocker/SettingsActivity.java
+++ app/src/main/java/darthleonard/archaos/playblocker/SettingsActivity.java
@@ -6,10 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import darthleonard.archaos.playblocker.database.DBAuthManager;
 
 public class SettingsActivity extends AppCompatActivity {
+    TextView tvPasswordMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +20,13 @@ public class SettingsActivity extends AppCompatActivity {
 
         findViewById(R.id.btnPasswordUpdate).setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
+                tvPasswordMessage = findViewById(R.id.tvPasswordMessage);
+                tvPasswordMessage.setVisibility(View.INVISIBLE);
                 EditText etPasswordNew = findViewById(R.id.etPasswordNew);
                 EditText etPasswordConfirm = findViewById(R.id.etPasswordConfirm);
                 String passwordNew = etPasswordNew.getText().toString();
                 String passwordConfirm = etPasswordConfirm.getText().toString();
-                if(passwordNew.equals(passwordConfirm)) {
+                if(validatePassword(passwordNew, passwordConfirm)) {
                     DBAuthManager db = new DBAuthManager(getApplicationContext());
                     db.Open();
                     db.SavePassword(passwordNew);
@@ -34,8 +39,26 @@ public class SettingsActivity extends AppCompatActivity {
                     etPasswordConfirm.setText("");
                     findViewById(R.id.tvPasswordMessage).setVisibility(View.VISIBLE);
                 }
-
             }
         });
+    }
+
+    public boolean validatePassword(String passwordNew, String passwordConfirm) {
+        if(passwordNew.length() < 3) {
+            tvPasswordMessage.setText(R.string.passwordShort);
+            tvPasswordMessage.setVisibility(View.VISIBLE);
+            return false;
+        }
+        if(passwordNew.length() < 3 || passwordNew.length() > 10) {
+            tvPasswordMessage.setText(R.string.passwordLong);
+            tvPasswordMessage.setVisibility(View.VISIBLE);
+            return false;
+        }
+        if(!passwordNew.equals(passwordConfirm)) {
+            tvPasswordMessage.setText(R.string.passwordMismatch);
+            tvPasswordMessage.setVisibility(View.VISIBLE);
+            return false;
+        }
+        return true;
     }
 }
