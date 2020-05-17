c4ed45b5e2cd45408f33b86e851ae569e06b58c4
darthleonard <leolinuxmx@gmail.com>
2020-05-16 23:24:16 -0500

    apps sorted

--- app/src/main/java/darthleonard/archaos/playblocker/model/AppLoader.java
+++ app/src/main/java/darthleonard/archaos/playblocker/model/AppLoader.java
@@ -4,6 +4,8 @@ import android.content.pm.ApplicationInfo;
 import android.os.AsyncTask;
 import android.view.ViewManager;
 
+import java.util.Comparator;
+
 public class AppLoader extends AsyncTask<Void, AppModel, Void> {
     private AppLoaderArgs args;
 
@@ -32,6 +34,12 @@ public class AppLoader extends AsyncTask<Void, AppModel, Void> {
         args.ProgressBar.setProgress(args.ProgressBar.getProgress() + 1);
         if(item != null) {
             args.Adapter.add(item[0]);
+            args.Adapter.sort(new Comparator<AppModel>() {
+                @Override
+                public int compare(AppModel o1, AppModel o2) {
+                    return o1.getAppName().compareTo(o2.getAppName());
+                }
+            });
         }
     }
 
