3065051bc4ed9ea9029ad5396df06ae093ecc8d5
darthleonard <leolinuxmx@gmail.com>
2020-07-26 01:26:57 -0500

    unused imports removed

--- app/src/main/java/darthleonard/archaos/playblocker/AccessActivity.java
+++ app/src/main/java/darthleonard/archaos/playblocker/AccessActivity.java
@@ -4,15 +4,9 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
